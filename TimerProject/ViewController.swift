f99db760dae46d1b5b688914b4fe3abe791a0c3c
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-12 23:58:06 +0300

    first commit

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
+        
     }
+    
+    @objc func timerFunction() {
+        
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time's Over"
+        }
+        
+        
+    }
+    
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        print("button clicked")
+    }
+    
 }
 
